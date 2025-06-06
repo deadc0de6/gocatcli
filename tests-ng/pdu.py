25d9bb6fe7c529ad26ec22c5c2fb85a2fc814b39
deadc0de6 <8973919+deadc0de6@users.noreply.github.com.>
2025-06-06 22:42:11 +0200

    improve pdu

--- tests-ng/pdu.py
+++ tests-ng/pdu.py
@@ -68,19 +68,37 @@ def main(path: str, human: bool,
         if must_ignore(root, ign):
             debug(f'ignore root {root}')
             continue
+        files.sort(key=lambda f: f[0] == '.')
         for file in files:
             fpath = os.path.join(root, file)
             if must_ignore(fpath, ign):
                 debug(f'ignore sub {fpath}')
                 continue
-            size = os.path.getsize(fpath)
+            # size = os.path.getsize(fpath)
+            stat_info = os.stat(fpath)
+            size = stat_info.st_size
             dirsz += size
             total += size
-        if root != path:
-            print(f'{size_to_str(dirsz, human=human, digits=1)} {root}')
-    print(f'{size_to_str(total, human=human)} {path}')
+        if root != path and dirsz > 0:
+            print_line(dirsz, get_norm_path(root, path), human, digits=1)
+    print_line(total, get_norm_path(path, path), human)
     return True
 
+def get_norm_path(path: str, base: str) -> str:
+    npath = os.path.normpath(path)
+    bnpath = os.path.normpath(base)
+    bbasename = os.path.basename(bnpath)
+    rel = os.path.relpath(npath, bnpath)
+    if rel == '.':
+        return bbasename
+    return os.path.join(bbasename, rel)
+
+def print_line(size, path, human, digits=None):
+    hsize = size_to_str(size, human=human, digits=digits)
+    if not human:
+        print(f'{hsize:<10}{path}')
+        return
+    print(f'{hsize:<10}{path}')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog=NAME,
